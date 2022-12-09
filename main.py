dcb0916567c59748b41435c2966c72a0c4a00552
franklinnazareno <franklinnazareno@gmail.com>
2022-12-09 07:24:44 +0000

    revised to jwt_required and made use of isL2 role

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from flask import Flask, Response, request, jsonify
+from flask_jwt_extended import jwt_required, JWTManager, create_access_token, get_jwt_identity
 from pymongo import MongoClient
 from ncclient import manager
 from ncclient.operations import RPCError
-import hashlib, jwt, xmltodict, json, requests 
+from bson import json_util
+from bson.objectid import ObjectId
+import hashlib, jwt, xmltodict, json, requests
+from datetime import datetime 
 
 # Define a secret key for signing the JWT
 secret_key = 'secret-key'
 
 # Create a Flask app
 app = Flask(__name__)
+
+# Create a JWTManager object
+jwt = JWTManager(app)
+
+# Initialize the JWTManager object with the Flask application
+jwt.init_app(app)
+
+app.config['JWT_SECRET_KEY'] = 'Devasc Finals Case Study'
 
 # Connect to MongoDB Atlas
 client = MongoClient("mongodb+srv://group-3-software-design:YCpTgKjSJYY17sBF@consultsched.81dxtxh.mongodb.net/?retryWrites=true&w=majority")
@@ -57,7 +69,7 @@ def change_hostname_using_netconf(hostname):
         # Send an <edit-config> NETCONF operation
         try:
             result = m.edit_config(target='running', config=config)
-            message = 'Attention: A member of L1 SE Team changed the hostname of the router to {}'.format(hostname)
+            message = 'A member of L2 NE Team has approved or changed the hostname of the router to {}'.format(hostname)
             url = 'https://webexapis.com/v1/messages'
             headers = {
                 'Authorization': 'Bearer {}'.format(access_token),
@@ -99,7 +111,7 @@ def add_loopback_using_netconf(name, description, address, mask):
         # Send an <edit-config> NETCONF operation
         try:
             result = m.edit_config(target='running', config=config)
-            message = 'Attention: A member of L1 SE Team has added Loopback{} with an address of {} {} and a description of "{}"'.format(name, address, mask, description)
+            message = 'A member of L2 NE Team has approved or added / change Loopback{} with an address and mask of {} {}, and a description of "{}"'.format(name, address, mask, description)
             url = 'https://webexapis.com/v1/messages'
             headers = {
                 'Authorization': 'Bearer {}'.format(access_token),
@@ -112,6 +124,21 @@ def add_loopback_using_netconf(name, description, address, mask):
         except RPCError as e:
             print(f'NETCONF operation failed: {e}')
             return None
+
+def authenticate_user(email, password):
+    # Check if the email exists in the database
+    user = db.users.find_one({'email': email})
+    if not user:
+        return jsonify({'message': 'Invalid email or password'}), 401
+
+    # Use SHA-256 to encrypt the password
+    password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+    # Check if the password is correct
+    if password_hash != user['password']:
+        return jsonify({'message': 'Invalid email or password'}), 401
+    
+    return user is not None
 
 # Define the route for the registration page
 @app.route('/register', methods=['POST'])
@@ -146,63 +173,86 @@ def login():
     email = request.json['email']
     password = request.json['password']
 
-    # Check if the email exists in the database
-    user = db.users.find_one({'email': email})
-    if not user:
-        return jsonify({'message': 'Invalid email or password'}), 401
+    if authenticate_user(email, password):
+        # Generate a JWT
+         token = create_access_token(identity=email)
+    else:
+        return jsonify({'message': 'Invalid username or password'}), 401
 
-    # Use SHA-256 to encrypt the password
-    password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
-
-    # Check if the password is correct
-    if password_hash != user['password']:
-        return jsonify({'message': 'Invalid email or password'}), 401
-
-    # Generate an authentication token
-    token = jwt.encode({'user': user['email']}, secret_key, algorithm='HS256')
-
-    # Return the token to the user
-    return jsonify({'token': token}), 200
+    # Return the JWT to the user
+    return jsonify({'token': token})
 
 @app.route('/show_running_config', methods=['GET'])
+@jwt_required()
 def show_running_config():
-    # Check if the user is authenticated
-    if not request.headers.get('Authorization'):
-        return jsonify({'message': 'Unauthorized'}), 401
-
-    # Use the jwt module to decode the token
-    try:
-        token = request.headers['Authorization'].split()[1]
-        user = jwt.decode(token, secret_key, algorithms=['HS256'])
-    except jwt.ExpiredSignatureError:
-        return jsonify({'message': 'Token expired'}), 401
-    except jwt.InvalidTokenError:
-        return jsonify({'message': 'Invalid token'}), 401
-
     # Use NETCONF to get the running configuration from the router
     running_config = get_running_config_using_netconf()
 
     # Return the running configuration to the user
     return running_config, 200
 
-@app.route('/change_hostname', methods=['POST'])
-def change_hostname():
-    # Check if the user is authenticated
-    if not request.headers.get('Authorization'):
-        return jsonify({'message': 'Unauthorized'}), 401
+@app.route('/changes', methods=['GET'])
+@jwt_required()
+def get_changes():
+    user = db.users.find_one({'email': get_jwt_identity()})
+    if (user['isL2'] == False):
+        return jsonify({'message': 'This can only be viewed by L2 Network Engineers.'}), 401
+    
+    changes = db.changes.find()
+    changesJson = json_util.dumps(changes)
 
-    # Use the jwt module to decode the token
-    try:
-        token = request.headers['Authorization'].split()[1]
-        user = jwt.decode(token, secret_key, algorithms=['HS256'])
-    except jwt.ExpiredSignatureError:
-        return jsonify({'message': 'Token expired'}), 401
-    except jwt.InvalidTokenError:
-        return jsonify({'message': 'Invalid token'}), 401
+
+    return changesJson,200
+
+@app.route('/changes', methods=['POST'])
+@jwt_required()
+def approve_changes():
+    user = db.users.find_one({'email': get_jwt_identity()})
+    if (user['isL2'] == False):
+        return jsonify({'message': 'This can only be viewed by L2 Network Engineers.'}), 401
+    change = db.changes.find_one({"_id": ObjectId(request.args['changes'])})
+    if (change['changes'] == "change_hostname"):
+        success = change_hostname_using_netconf(change['hostname'])
+        if (success):
+            change = db.changes.find_one_and_delete({"_id": ObjectId(request.args['changes'])})
+            return success, 200
+        else:
+            return jsonify({'message': 'Failed to change hostname'}), 500
+    elif (change['changes'] == "add_loopback"):
+        success = add_loopback_using_netconf(change['name'], change['description'], change['address'], change['mask'])
+        if (success):
+            change = db.changes.find_one_and_delete({"_id": ObjectId(request.args['changes'])})
+            return success, 200
+        else:
+            return jsonify({'message': 'Failed to add loopback address'}), 500
+    
+
+@app.route('/change_hostname', methods=['POST'])
+@jwt_required()
+def change_hostname():
+    # Get the logged in user's claims
+    user = db.users.find_one({'email': get_jwt_identity()})
 
     # Get the new hostname from the request body
     hostname = request.json['hostname']
 
+    if (user['isL2'] == False):
+        change = {
+            "changes": "change_hostname",
+            "hostname": hostname,
+            "date": datetime.now()
+        }
+        db.changes.insert_one(change)
+        message = 'A member of L1 SE Team is requesting to change the hostname to "{}"'.format(hostname)
+        url = 'https://webexapis.com/v1/messages'
+        headers = {
+            'Authorization': 'Bearer {}'.format(access_token),
+            'Content-Type': 'application/json'
+        }
+        params = {'roomId': room_id, 'markdown': message}
+        res = requests.post(url, headers=headers, json=params)
+        print(res.json())
+        return jsonify({'message': 'Your change request have been sent to the L2 Network Engineers.'}), 200
     # Use NETCONF to change the hostname of the router
     success = change_hostname_using_netconf(hostname)
 
@@ -213,19 +263,10 @@ def change_hostname():
         return jsonify({'message': 'Failed to change hostname'}), 500
 
 @app.route('/add_loopback', methods=['POST'])
+@jwt_required()
 def add_loopback():
-    # Check if the user is authenticated
-    if not request.headers.get('Authorization'):
-        return jsonify({'message': 'Unauthorized'}), 401
-
-    # Use the jwt module to decode the token
-    try:
-        token = request.headers['Authorization'].split()[1]
-        user = jwt.decode(token, secret_key, algorithms=['HS256'])
-    except jwt.ExpiredSignatureError:
-        return jsonify({'message': 'Token expired'}), 401
-    except jwt.InvalidTokenError:
-        return jsonify({'message': 'Invalid token'}), 401
+    # Get the logged in user's claims
+    user = db.users.find_one({'email': get_jwt_identity()})
 
     # Get the loopback address and mask from the request body
     name = request.json['name']
@@ -236,6 +277,27 @@ def add_loopback():
     # Check if name is a number
     if type(int(name)) != int:
         return jsonify({'message': 'Enter a numbered name'}), 401
+
+    if (user['isL2'] == False):
+        change = {
+            "changes": "add_loopback",
+            "name": name,
+            "description": description,
+            "address": address,
+            "mask": mask,
+            "date": datetime.now()
+        }
+        db.changes.insert_one(change)
+        message = 'A member of L1 SE Team is requesting to add or change Loopback{} with the address and mask of {} {}, and a description of "{}"'.format(name, address, mask, description)
+        url = 'https://webexapis.com/v1/messages'
+        headers = {
+            'Authorization': 'Bearer {}'.format(access_token),
+            'Content-Type': 'application/json'
+        }
+        params = {'roomId': room_id, 'markdown': message}
+        res = requests.post(url, headers=headers, json=params)
+        print(res.json())
+        return jsonify({'message': 'Your change request have been sent to the L2 Network Engineers.'}), 200
 
     # Use NETCONF to add the loopback address to the router
     success = add_loopback_using_netconf(name, description, address, mask)
